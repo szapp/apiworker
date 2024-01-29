bd79868a993df70f59d040ba5bf722d8562cd36a
szapp <dev.szapp@gmail.com>
2024-01-29 16:31:48 +0100

    Update spine download counts

--- src/downloads/projects.ts
+++ src/downloads/projects.ts
@@ -1,7 +1,7 @@
 export const projects: { [key: string]: [ number | string ] } = {
   ninja: {
     github: 'szapp/Ninja',
-    spine: 144150,
+    spine: 145424, // ID 314
     steam: [
       2786936496,
       2786910489,
@@ -10,17 +10,17 @@ export const projects: { [key: string]: [ number | string ] } = {
   },
   g1cp: {
     github: 'AmProsius/gothic-1-community-patch',
-    spine: 14683,
+    spine: 14882, // ID 395
     steam: 2789245548,
     wog: 660,
   },
   gfa: {
     github: 'szapp/GothicFreeAim',
     spine: [
-      2854,
-      3647,
-      920,
-      6886,
+      2869, // ID 192
+      3669, // ID 75
+      920,  // ID 227
+      6941, // ID 223
     ],
     steam: [
       2786959658,
