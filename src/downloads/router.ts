6359b001398a6579392754b41bf727ad252ca3ee
Sören Zapp <dev.szapp@gmail.com>
2024-02-05 10:03:41 +0100

    Add download specifications to router

--- src/downloads/router.ts
+++ src/downloads/router.ts
@@ -65,6 +65,19 @@ router.get('/:project/:service/:badge?', async (request, env, ctx) => {
   return invalidRoute()
 })
 
+router.get('/', async (request, env, ctx) => {
+	const url = new URL(request.url)
+  return new Response(
+    JSON.stringify({
+			downloads_url: `${url.origin}/downloads/{project_id}/{service_id}`,
+			badge_url: `${url.origin}/downloads/{project_id}/{service_id}/badge{?label,color,style,logo,logoColor}`,
+			service_ids: services.concat('total'),
+			project_ids: Object.keys(projects)
+		}),
+    { status: 200, headers: { 'Content-Type': 'application/json; charset=utf-8' }
+  })
+})
+
 // 404 for everything else
 router.all('*', invalidRoute)
 
