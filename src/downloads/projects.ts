c7d26bc278e099affcb07d4435a6e1997c9791c9
Sören Zapp <dev.szapp@gmail.com>
2025-08-20 22:04:48 +0200

    Update download counts

--- src/downloads/projects.ts
+++ src/downloads/projects.ts
@@ -1,23 +1,23 @@
 export const projects: Record<string, Record<string, string | number | (string | number)[]>> = {
   ninja: {
     github: 'szapp/Ninja',
-    spine: 179628, // ID 314
+    spine: 181758, // ID 314
     steam: [2786936496, 2786910489],
     wog: 652,
   },
   g1cp: {
     github: 'AmProsius/gothic-1-community-patch',
-    spine: 18776, // ID 395
+    spine: 18969, // ID 395
     steam: 2789245548,
     wog: 660,
   },
   gfa: {
     github: ['szapp/GothicFreeAim', 'szapp/FreeAiming'],
     spine: [
-      3339, // ID 192
-      4133, // ID 75
+      3336, // ID 192
+      4163, // ID 75
       920, // ID 227 (disabled)
-      8490, // ID 223
+      8574, // ID 223
     ],
     steam: [2786959658, 2786958841],
     wog: [548, 535, 613],
