ec189fa6db9240fbf8775235f7720c71373b1e37
szapp <dev.szapp@gmail.com>
2025-09-03 22:35:07 +0200

    Fix participants count by data

--- src/participants/services/github.ts
+++ src/participants/services/github.ts
@@ -35,7 +35,7 @@ async function getUsersFetch(url: string, env: Env): Promise<UserInfo> {
     })
       .then(async (response) => {
         if (!response.ok) Promise.reject()
-        const issuesData: { user: { login: string; html_url: string; avatar_url: string }; created_at: string }[] = await (
+        const issuesData: { user: { login: string; html_url: string; avatar_url: string }; updated_at: string }[] = await (
           response as unknown as Response
         ).json()
         for (const issue of issuesData) {
@@ -45,7 +45,7 @@ async function getUsersFetch(url: string, env: Env): Promise<UserInfo> {
             image: issue.user.avatar_url,
             contributions: 1,
           })
-          const date = Date.parse(issue.created_at)
+          const date = Date.parse(issue.updated_at)
           if (date > latestDate) latestDate = date
         }
         const link: string | null = response.headers.get('link')
